18e04cd7e2da1be1865dcd30e94e0c5e73f0be3a
Tikuochukwu Okoye <okoyetiku@gmail.com>
2019-04-24 13:01:43 +0100

    task: packer AMI config with Ansible

---
+++ scripts/ansible.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+#Installs necessary repos and packages to set up ansible
+sudo apt-get update -y
+sudo apt-get install software-properties-common
+sudo apt-add-repository --yes --update ppa:ansible/ansible
+sudo apt-get install -y ansible
