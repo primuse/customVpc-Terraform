b9bd694d7ac6a1200e698c5a9fe124431d54da26
Tikuochukwu Okoye <okoyetiku@gmail.com>
2019-04-25 08:38:58 +0100

    task: refactor scripts

--- scripts/ansible.sh
+++ scripts/ansible.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 
 #Installs necessary repos and packages to set up ansible
-sudo apt-get update -y
-sudo apt-get install software-properties-common
-sudo apt-add-repository --yes --update ppa:ansible/ansible
-sudo apt-get install -y ansible
+function installAnsible {
+  sudo apt-get update -y
+  sudo apt-get install software-properties-common
+  sudo apt-add-repository --yes --update ppa:ansible/ansible
+  sudo apt-get install -y ansible
+}
+
+installAnsible
