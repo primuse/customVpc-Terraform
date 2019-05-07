ab6d89cb8b6fde25f5d76cca341da8f06a48f3d8
Tikuochukwu Okoye <okoyetiku@gmail.com>
2019-05-07 16:55:16 +0100

    task:  automate ami build and deployment

---
+++ scripts/deploy.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+
+accessKey=$1
+secretKey=$2
+
+#Creates the Api AMI
+function createApiAmi {
+  cd ..
+  export AWS_ACCESS_KEY_ID=$accessKey
+  export AWS_SECRET_ACCESS_KEY=$secretKey
+  packer build api.json
+}
+
+#Creates the database AMI
+function createDatabaseAmi {
+  export AWS_ACCESS_KEY_ID=$accessKey
+  export AWS_SECRET_ACCESS_KEY=$secretKey
+  packer build database.json
+}
+
+#Creates the frontend AMI
+function createFrontendAmi {
+  export AWS_ACCESS_KEY_ID=$accessKey
+  export AWS_SECRET_ACCESS_KEY=$secretKey
+  packer build frontend.json
+}
+
+Builds infrastructure and Launches instances with Terraform
+function terraformLaunch {
+  cd terraform
+  terraform init -input=false
+  terraform validate
+  terraform plan -out=tfplan -input=false
+  terraform apply -input=false tfplan
+}
+
+function main {
+  createApiAmi
+  createDatabaseAmi
+  createFrontendAmi
+  terraformLaunch
+}
+
+#Calls the main function
+main
+
